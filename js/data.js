b05b07824553d26e0c4642f24bee6a2cc06718ea
Alfredo Sena <33937487+avsena@users.noreply.github.com>
2022-10-18 11:46:18 -0300

    Deletando linhas de comentários de teste

--- js/data.js
+++ js/data.js
@@ -1,15 +1,3 @@
-/*setInterval(() => {
-    
-    var dataCurrent = new Date();
-
-    meses = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
-
-    dias = new Array('Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado');
-
-    document.querySelector('.data').innerHTML = dias[dataCurrent.getDay()] + ', ' +
-        (fix(dataCurrent.getDate()) + ' de ' + meses[dataCurrent.getMonth()] + ' de ' + dataCurrent.getFullYear());
-}, 1000);*/
-
 setInterval(() => {
 
     let dataCurrent = new Date();
